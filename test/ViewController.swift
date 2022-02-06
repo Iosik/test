4dd84740a7c0ceef3935726f3e1231b02726f545
Iosik <9903633+Iosik@users.noreply.github.com>
2022-02-06 00:16:49 -0600

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by максим  кондратьев  on 06.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
